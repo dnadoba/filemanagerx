16146d462e03665f666d519ade9bbc50f56e16a4
Max <max.stockbauer1@web.de>
2018-03-16 15:49:25 +0100

    fixin

--- routes/admin/list.js
+++ routes/admin/list.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const files = require('../../api/files')
+const urljoin = require('url-join')
 
 router.get('/admin/list*', (req, res, next) => {
   let directoryPath = path.join(__dirname, '../../files', req.params[0])
 
   files.list(directoryPath, req.params[0], (err, data) => {
 
-    res.locals.path = path
+    res.locals.urljoin = urljoin
     //    res.locals.admin = true
 
     res.render('files/list', {
