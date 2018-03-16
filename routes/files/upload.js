6e8fb43470d32aa56798b132c12c558c960a494a
David Nadoba <dnadoba@gmail.com>
2018-03-16 11:13:04 +0100

    Upload done
    created the upload magic
    
    Co-Authored-By: SeJV <sejv@users.noreply.github.com>
    Co-Authored-By: Ben <1622998@users.noreply.github.com>

--- routes/files/upload.js
+++ routes/files/upload.js
@@ -1,8 +1,29 @@
+const path = require('path')
+const os = require('os')
+const fs = require('fs')
 const express = require('express')
 const router = express.Router()
+const multer  = require('multer')
+const upload = multer({ dest: path.join(os.tmpdir(), 'uploads') })
+const destinationPath = path.join(path.dirname(require.main.filename), 'files')
 
-router.post('/files/upload', (req, res, next) => {
-  
+fs.mkdirSync(destinationPath)
+
+router.get('/files/upload', (req, res, next) => {
+  res.render("uploads")
+})
+
+router.post('/files/upload', upload.any(),(req, res, next) => {
+  req.files.forEach((file)=>{
+    fs.rename(file.path, path.join(destinationPath, file.originalname), (error)=>{
+      if (error) {
+        console.error(error)
+      }
+    })
+  })
+  console.log(req.files)
+
+  res.render("uploads")
 })
 
 module.exports = router
