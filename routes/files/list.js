e9daf7396a4593bf7914c70c66d57986578ca3eb
David Nadoba <dnadoba@gmail.com>
2018-03-16 14:19:55 +0100

    delete
    Delete funktioniert jetzt
    
    Co-Authored-By: SeJV <sejv@users.noreply.github.com>

--- routes/files/list.js
+++ routes/files/list.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
 const path = require('path')
 const files = require('../../api/files')
 
 router.get('/list*', (req, res, next) => {
-  console.log(__dirname)
-
   let directoryPath = path.join(__dirname, '../../files', req.params[0])
 
   files.list(directoryPath, req.params[0], (err, data) => {
