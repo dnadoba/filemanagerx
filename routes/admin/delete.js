63c5d0027dc72b62bfb10b9c74f4dee0bc80c239
David Nadoba <dnadoba@gmail.com>
2018-03-19 13:51:45 +0100

    delete file async and ignore file not found error

--- routes/admin/delete.js
+++ routes/admin/delete.js
@@ -7,21 +7,32 @@ const urljoin = require('url-join')
 
 router.post('/admin/list*', (req, res, next) => {
   let directoryPath = path.join(__dirname, '../../files', req.params[0])
-
-  fs.unlinkSync(path.join(path.dirname(require.main.filename), req.body.path))
-
-  files.list(directoryPath, req.params[0], (err, data) => {
-    if(err) {
-      return next(err)
+  let filePath = path.join(path.dirname(require.main.filename), req.body.path)
+  fs.unlink(filePath, (error) => {
+    if(error) {
+      //file does not exists
+      if (error.code === "ENOENT") {
+        //ignore, it was probably already delete
+      } else {
+        return next(error)
+      }
     }
-    res.locals.urljoin = urljoin
 
-    res.render('files/list', {
-      data: data,
-      admin: true,
-      pagePath: '/admin/list',
-    });
+    files.list(directoryPath, req.params[0], (err, data) => {
+      if(err) {
+        return next(err)
+      }
+      res.locals.urljoin = urljoin
+
+      res.render('files/list', {
+        data: data,
+        admin: true,
+        pagePath: '/admin/list',
+      });
+    })
   })
+
+
 })
 
 module.exports = router
