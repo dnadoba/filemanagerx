2b80e13f53ae3be3df70ebee158964e0724441d3
David Nadoba <dnadoba@gmail.com>
2018-03-20 17:47:51 +0100

    implement create new folder

--- routes/admin/createFolder.js
+++ routes/admin/createFolder.js
@@ -1,9 +1,25 @@
+const fs = require('fs')
+const path = require('path')
 
+let rootFilesFolderPath = path.join(path.dirname(require.main.filename), '/files')
 
 function createFolder(req, res, next) {
   const currentRelativeFolderPath = req.params[0]
   const folderName = req.body.folderName
-  next(new Error("createFolder not yet implemented"))
+
+  const folderPath = path.join(rootFilesFolderPath, currentRelativeFolderPath, folderName)
+
+  fs.mkdir(folderPath, function(error) {
+    if(error) {
+      //the folder does already exists
+      if(error.code === 'EEXIST') {
+        //ignore
+      } else {
+        return next(error)
+      }
+    }
+    next()
+  })
 }
 
 module.exports = {
